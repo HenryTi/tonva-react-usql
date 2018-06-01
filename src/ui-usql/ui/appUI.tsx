beb3c5a7cf95a19a38c20aef2e4ed71106b74e47
ligsh <system6@jkchemical.com>
2018-06-01 15:15:43 +0800

    fix EntitiesUI.mergeListPage error

--- src/ui-usql/ui/appUI.tsx
+++ src/ui-usql/ui/appUI.tsx
@@ -41,7 +41,7 @@ export class AppUI {
             let api = apiOwner + '/' + apiName;
             let mapper = this.uiMappers && this.uiMappers[api];
             if (mapper === null) continue;
-            // if (isDebug === true && urlDebug !== undefined) url = urlDebug;
+            if (isDebug === true && urlDebug !== undefined) url = urlDebug;
             let apiUI = new EntitiesUI(url, ws, api, access, defaultMapper, mapper);
             this.apiUIs.push(apiUI);
             await apiUI.loadEntities();
