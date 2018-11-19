af47d102f7405a9eb1662f0b393ae1aa8125da57
Henry <liaohengyi@outlook.com>
2018-11-19 14:03:51 +0800

    push CApp

---
+++ dist/ui-usql/controllers/CApp.d.ts
@@ -0,0 +1,39 @@
+import { Controller, TypeVPage } from 'tonva-tools';
+import { CUsq, UsqUI } from './usq';
+export interface AppUI {
+    CApp?: typeof CApp;
+    CUsq?: typeof CUsq;
+    main?: TypeVPage<CApp>;
+    usqs: {
+        [usq: string]: UsqUI;
+    };
+    res?: any;
+}
+export declare class CApp extends Controller {
+    private appOwner;
+    private appName;
+    private isProduction;
+    protected ui: AppUI;
+    id: number;
+    appUnits: any[];
+    constructor(tonvaApp: string, ui: AppUI);
+    readonly caption: string;
+    cUsqCollection: {
+        [usq: string]: CUsq;
+    };
+    protected loadUsqs(): Promise<string[]>;
+    protected newCUsq(usq: string, usqId: number, access: string, ui: any): CUsq;
+    readonly cUsqArr: CUsq[];
+    getCUsq(apiName: string): CUsq;
+    protected readonly VAppMain: TypeVPage<CApp>;
+    protected beforeStart(): Promise<boolean>;
+    protected internalStart(): Promise<void>;
+    protected clearPrevPages(): void;
+    private showUnsupport;
+    private showMainPage;
+    private getCUsqFromId;
+    private loadAppUnits;
+    renderRow: (item: any, index: number) => JSX.Element;
+    onRowClick: (item: any) => Promise<void>;
+    protected selectUnitPage: () => JSX.Element;
+}
