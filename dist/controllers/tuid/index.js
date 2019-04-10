496582252584efd1a8ee73badcbeadaff49a32f1
Henry <liaohengyi@outlook.com>
2019-04-10 16:58:06 -0400

    export VTuidSelect

--- dist/controllers/tuid/index.js
+++ dist/controllers/tuid/index.js
@@ -3,4 +3,5 @@ export * from './vTuidMain';
 export * from './vTuidEdit';
 export * from './vTuidInfo';
 export * from './vTuidView';
+export * from './vTuidSelect';
 //# sourceMappingURL=index.js.map
